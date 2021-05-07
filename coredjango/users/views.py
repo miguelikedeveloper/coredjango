22bdb02817c17e9279970944e23e23d3487af27c
miguelikedeveloper <miguelrg72@yahoo.es>
2021-05-07 21:02:34 +0200

    User Registration

--- coredjango/users/views.py
+++ coredjango/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+from .forms import RegistratrionForm
+
+def registration_view(request):
+    context = {}
+    if request.POST:
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email  = form.cleaned_data.get("form")
+            raw_password  = form.cleaned_data.get("password1")
+            account = authenticate(email=email, password=password1)
+            login(request, account)
+            return redirect("admin")
+
+        else:
+            context["registration_form"] = form
+
+    else:
+        form = RegistratrionForm()
+        context["registration_form"]  = form
+    return render(request, "users/register.html", context)
